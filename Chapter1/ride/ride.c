6fb86b8840afc026ae1ad2d7244a6b37810c50ea
nasi [Nathan Li] <nasiless@gmail.com>
2011-02-12 13:43:46 +0800

    Passed PROB ride

--- Chapter1/ride/ride.c
+++ Chapter1/ride/ride.c
@@ -5,8 +5,29 @@ TASK: ride
 */
 #include <stdio.h>
 
+int number(char *s)
+{
+    int ret = 1;
+    char *ch = s;
+    while (*ch) {
+        ret *= *ch - 64;
+        ch++;
+    }
+    return ret % 47;
+}
+
 int main()
 {
+    FILE *fin  = fopen ("ride.in", "r");
+    FILE *fout = fopen ("ride.out", "w");
+
+    char comet[10], group[10];
+    fscanf(fin, "%s", comet);
+    fscanf(fin, "%s", group);
+    if (number(comet) == number(group)) 
+        fprintf(fout, "GO\n");
+    else
+        fprintf(fout, "STAY\n");
 
     return 0;
 }
