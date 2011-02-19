99dc63cbc6f65b5618ebddd79806dc4d0b4c8124
nasi [Nathan Li] <nasiless@gmail.com>
2011-02-19 18:18:13 +0800

    Change tabs to spaces

--- chap1/checker/checker.c
+++ chap1/checker/checker.c
@@ -11,33 +11,33 @@ int ans = 0, cnt = 0;
 
 void solve(int i, int limit, FILE *fout)
 {
-	int j;
-	if (i == n) {
-		if (cnt++ < 3) {
-			fprintf(fout, "%d", p[0]+1);
-			for (j = 1; j < n; j++)
-				fprintf(fout, " %d", p[j]+1);
-			fprintf(fout, "\n");
-		}
-		ans++;
-		if (n > 6 && p[0] < n/2) ans++;
-	} else {
-		for (j = 0; j < limit; j++) {
-			if (!a[j] && !l[i+j] && !r[n-1-i+j]) {
-				a[j] = l[i+j] = r[n-1-i+j] = 1;
-				p[i] = j;
-				solve(i+1, n, fout);
-				a[j] = l[i+j] = r[n-1-i+j] = 0;
-			}
-		}
-	}
+    int j;
+    if (i == n) {
+        if (cnt++ < 3) {
+            fprintf(fout, "%d", p[0]+1);
+            for (j = 1; j < n; j++)
+                fprintf(fout, " %d", p[j]+1);
+            fprintf(fout, "\n");
+        }
+        ans++;
+        if (n > 6 && p[0] < n/2) ans++;
+    } else {
+        for (j = 0; j < limit; j++) {
+            if (!a[j] && !l[i+j] && !r[n-1-i+j]) {
+                a[j] = l[i+j] = r[n-1-i+j] = 1;
+                p[i] = j;
+                solve(i+1, n, fout);
+                a[j] = l[i+j] = r[n-1-i+j] = 0;
+            }
+        }
+    }
 }
 
 void init()
 {
-	memset(a, 0, sizeof(a));
-	memset(l, 0, sizeof(l));
-	memset(r, 0, sizeof(r));
+    memset(a, 0, sizeof(a));
+    memset(l, 0, sizeof(l));
+    memset(r, 0, sizeof(r));
 }
 
 int main()
