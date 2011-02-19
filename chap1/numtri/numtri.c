d42d512d1f5c6beeaa4cc8ff8610a8d0126653b3
nasi [Nathan Li] <nasiless@gmail.com>
2011-02-19 18:19:31 +0800

    Change tabs to spaces

--- chap1/numtri/numtri.c
+++ chap1/numtri/numtri.c
@@ -17,14 +17,14 @@ int main()
 
     fscanf(fin, "%d", &n);
     for (i = 0; i < n; i++) {
-    	for (j = 0; j <= i; j++) {
-    		fscanf(fin, "%d", &t);
-    		if (!i) a[i][j] = t; else {
-    			if (j == 0) a[i][j] = t + a[i-1][j];
-    			else if (j == i) a[i][j] = t + a[i-1][j-1];
-    			else a[i][j] = t + max(a[i-1][j-1], a[i-1][j]);
-    		}
-    	}
+        for (j = 0; j <= i; j++) {
+            fscanf(fin, "%d", &t);
+            if (!i) a[i][j] = t; else {
+                if (j == 0) a[i][j] = t + a[i-1][j];
+                else if (j == i) a[i][j] = t + a[i-1][j-1];
+                else a[i][j] = t + max(a[i-1][j-1], a[i-1][j]);
+            }
+        }
     }
     for (i = 0; i< n; i++) ans = max(ans, a[n-1][i]);
     fprintf(fout, "%d\n", ans);
