b1839f1ac2f437acad622d5cc9b851226feda316
nasi [Nathan Li] <nasiless@gmail.com>
2011-02-19 18:20:54 +0800

    Change tabs to spaces

--- chap1/pprime/pprime.c
+++ chap1/pprime/pprime.c
@@ -12,47 +12,47 @@ int np[100000];
 
 void init()
 {
-	int i, j;
-	memset(np, 0, sizeof(np));
-	np[0] = np[1] = 1;
-	for (i = 2; i < 100000; i++) {
-		if (!np[i])
-			for (j = i+i; j < 100000; j += i) np[j] = 1;
-	}
+    int i, j;
+    memset(np, 0, sizeof(np));
+    np[0] = np[1] = 1;
+    for (i = 2; i < 100000; i++) {
+        if (!np[i])
+            for (j = i+i; j < 100000; j += i) np[j] = 1;
+    }
 }
 
 int prime(int x)
 {
-	int i;
-	for (i = 2; i <= (int)sqrt(x); i++) if (x % i == 0) return 0;
-	return 1;
+    int i;
+    for (i = 2; i <= (int)sqrt(x); i++) if (x % i == 0) return 0;
+    return 1;
 }
 
 void solve(int ndigit)
 {
-	int i, j, k, l, s;
-	if (ndigit == 3) {
-		for (i = 1; i < 10; i += 2)
-			for (j = 0; j < 10; j++) {
-				s = i*100 + j*10 + i;
-				if (!np[s]) { ans[n++] = s; }
-			}
-	} else if (ndigit == 5) {
-		for (i = 1; i < 10; i += 2)
-			for (j = 0; j < 10; j++) 
-				for (k = 0; k < 10; k++) {
-					s = i*10000 + j*1000 + k*100 + j*10 + i;
-					if (!np[s]) { ans[n++] = s; }
-				}
-	} else if (ndigit == 7) {
-		for (i = 1; i < 10; i += 2)
-			for (j = 0; j < 10; j++)
-				for (k = 0; k < 10; k++)
-					for (l = 0; l < 10; l++) {
-						s = i*1000000 + j*100000 + k*10000 + l*1000 + k*100 + j*10 + i;
-						if (prime(s)) { ans[n++] = s; }
-					}
-	}
+    int i, j, k, l, s;
+    if (ndigit == 3) {
+        for (i = 1; i < 10; i += 2)
+            for (j = 0; j < 10; j++) {
+                s = i*100 + j*10 + i;
+                if (!np[s]) { ans[n++] = s; }
+            }
+    } else if (ndigit == 5) {
+        for (i = 1; i < 10; i += 2)
+            for (j = 0; j < 10; j++) 
+                for (k = 0; k < 10; k++) {
+                    s = i*10000 + j*1000 + k*100 + j*10 + i;
+                    if (!np[s]) { ans[n++] = s; }
+                }
+    } else if (ndigit == 7) {
+        for (i = 1; i < 10; i += 2)
+            for (j = 0; j < 10; j++)
+                for (k = 0; k < 10; k++)
+                    for (l = 0; l < 10; l++) {
+                        s = i*1000000 + j*100000 + k*10000 + l*1000 + k*100 + j*10 + i;
+                        if (prime(s)) { ans[n++] = s; }
+                    }
+    }
 }
 
 int main()
@@ -67,7 +67,7 @@ int main()
     solve(3); solve(5); solve(7);
     
     for (i = 0; i < n; i++) {
-    	if (ans[i] >= a && ans[i] <= b) fprintf(fout, "%d\n", ans[i]);
+        if (ans[i] >= a && ans[i] <= b) fprintf(fout, "%d\n", ans[i]);
     }
     return 0;
 }
